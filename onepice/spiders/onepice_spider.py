e79a2db4a5c9c663d8064d40bf1ab803487a5892
root <root@localhost.localdomain>
2016-12-13 06:08:26 +0800

    download html

--- onepice/spiders/onepice_spider.py
+++ onepice/spiders/onepice_spider.py
@@ -7,5 +7,13 @@ import zlib
 
 class OnepiceSpider(scrapy.Spider):
 	name = "onepice"
-
-	pass
+	
+	def start_requests(self):
+		urls = ['http://op.hanhande.com/mh/']
+		for url in urls:
+			yield scrapy.Request(url=url,callback=self.parse)
+	def parse(self,respone):
+		filename = 'onepice.html'
+		with open(filename,'wb') as f:
+			f.write(respone.body)
+		self.log('Saved filed %s' % filename)
